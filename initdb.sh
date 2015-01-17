b08838f42fae4b91f22f27d8a2b1aecaf8bd8791
Serban Teodorescu <teodorescu.serban@gmail.com>
2015-01-17 03:24:58 +0200

    fixed initdb script to connect to db host instead of localhost

--- initdb.sh
+++ initdb.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
-pg_restore -vOx -U cps -d cps /srv/cps_schema_0_5_16.plsql
+pg_restore -vOx -h db -U cps -d cps /srv/cps_schema_0_5_16.plsql
 
-psql -U cps cps -f /srv/cps-initial-user.sql
+psql -h db -U cps cps -f /srv/cps-initial-user.sql
