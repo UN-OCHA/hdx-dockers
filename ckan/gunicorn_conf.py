a6ddf310faff4f112122ce2e25adddd8c6100f12
Serban Teodorescu <teodorescu.serban@gmail.com>
2015-07-20 14:49:07 +0300

    ckan: tuneup gunicorn settings (added timeout 120, graceful timeout 90, changed workers from 2 to 5

--- ckan/gunicorn_conf.py
+++ ckan/gunicorn_conf.py
@@ -1,7 +1,9 @@
 user = 'www-data'
 group = 'www-data'
 bind = '0.0.0.0:9221'
-workers = 2
+workers = 5
 accesslog = '/var/log/ckan/ckan.access.log'
 errorlog = '/var/log/ckan/ckan.error.log'
 loglevel = 'warning'
+timeout = 120
+grafeful_timeout = 90
