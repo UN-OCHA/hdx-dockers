4619d47cff0194d35e7b32f4b571db23b4b7c17d
Serban Teodorescu <teodorescu.serban@gmail.com>
2015-12-07 15:50:33 +0200

    dataproxy: changed log format to account for x-forwarded-for header instead of simple remote_addr.

--- dataproxy/gunicorn_conf.py
+++ dataproxy/gunicorn_conf.py
@@ -2,6 +2,7 @@ user = 'www-data'
 group = 'www-data'
 bind = '0.0.0.0:5000'
 workers = 2
+access_log_format = '"%({x-forwarded-for}i)s" %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 accesslog = '/var/log/dataproxy.access.log'
 errorlog = '/var/log/dataproxy.error.log'
 loglevel = 'debug'
