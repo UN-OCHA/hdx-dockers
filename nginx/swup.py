068b9eb3c3ae488b0e9a552efdb7f3ce5570f733
Serban Teodorescu <teodorescu.serban@gmail.com>
2016-02-06 01:05:46 +0200

    nginx: added swup.py

---
+++ nginx/swup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+"""make it easier to switch upstream."""
+
+import subprocess
+import sys
+
+# UPSTREAMS = {'blue': '10.0.0.1', 'green': '10.0.0.2'}
+UPSTREAMS = {'blue': '10.5.101.24', 'green': '10.5.102.24'}
+
+
+CFG = '/etc/nginx/conf.d/upstreams.conf'
+
+
+def figure_current_upstream():
+    """figure the current upstream."""
+    if UPSTREAMS['blue'] not in content:
+        if UPSTREAMS['green'] in content:
+            return 'green'
+    if UPSTREAMS['green'] not in content:
+        if UPSTREAMS['blue'] in content:
+            return 'blue'
+
+
+def figure_next_upstream():
+    """figure out the next upstream."""
+    if current_upstream == 'blue':
+        return 'green'
+    if current_upstream == 'green':
+        return 'blue'
+
+
+def switch_upstream():
+    """switch the upstream."""
+    new_content = content.replace(UPSTREAMS[current_upstream],
+                                  UPSTREAMS[next_upstream])
+    with open(CFG, 'w') as f:
+        f.write(new_content)
+
+
+with open(CFG) as f:
+    content = f.read()
+
+current_upstream = figure_current_upstream()
+next_upstream = figure_next_upstream()
+
+if not current_upstream:
+    print 'Unknown configuration.'
+    print "You forgot to manually choose the colors' values?"
+    print 'Exiting.'
+    sys.exit(1)
+
+print 'Switching from %s (%s) to %s (%s)' % (current_upstream,
+                                             UPSTREAMS[current_upstream],
+                                             next_upstream,
+                                             UPSTREAMS[next_upstream])
+switch_upstream()
+subprocess.call(['nginx', '-t'])
+print 'Assuming you got "ok" above, just type:', 'nginx -s reload'
