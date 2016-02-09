8324fb5af727a49d655773ca032d77c706617ad4
Serban Teodorescu <teodorescu.serban@gmail.com>
2016-02-09 11:25:20 +0200

    cps: optimize dockerfile; migrate to new service startup.

--- cps/initdb.sh
+++ cps/initdb.sh
@@ -1,5 +1,9 @@
 #/bin/bash
 
-pg_restore -vOx -h ${HDX_CPSDB_ADDR} -p ${HDX_CPSDB_PORT} -U ${HDX_CPSDB_USER} -d ${HDX_CPSDB_DB} /srv/cps_schema_0_5_16.plsql
-
-psql -h ${HDX_CPSDB_ADDR} -p ${HDX_CPSDB_PORT} -U ${HDX_CPSDB_USER} ${HDX_CPSDB_DB} -f /srv/cps-initial-user.sql
+# create cps schema and a default user if we have an empty database
+if [ "$(psql -h ${HDX_CPSDB_ADDR} -p ${HDX_CPSDB_PORT} -U cps cps -c '\d')" == "No relations found." ]; then
+    echo "Create empty cps schema"
+    pg_restore -vOx -h ${HDX_CPSDB_ADDR} -p ${HDX_CPSDB_PORT} -U ${HDX_CPSDB_USER} -d ${HDX_CPSDB_DB} /srv/cps_schema_0_5_16.plsql
+    echo "Create example admin user"
+    psql -h ${HDX_CPSDB_ADDR} -p ${HDX_CPSDB_PORT} -U ${HDX_CPSDB_USER} ${HDX_CPSDB_DB} -f /srv/cps-initial-user.sql
+fi
