7eb37ac3a6e199ac3dd45424798b21824457ab82
Serban Teodorescu <teodorescu.serban@gmail.com>
2016-04-14 17:41:44 +0300

    mpx: fix script

--- mpx/deploy.sh
+++ mpx/deploy.sh
@@ -3,22 +3,26 @@ set -e
 
 mkdir -p $SRC_DIR
 cd $SRC_DIR
-git status > /dev/null 2>&1
-if [ $? -eq 0 ]; then
-    git checkout $BRANCH
-    git pull
-else
-    rm -rf $SRC_DIR/*
-    git clone https://github.com/OCHA-DAP/liverpool16.git .
-    git checkout $BRANCH
-fi
+#git status > /dev/null 2>&1
+#if [ $? -eq 0 ]; then
+#    git checkout $BRANCH
+#    git pull
+#else
+#    rm -rf $SRC_DIR/*
+#    git clone https://github.com/OCHA-DAP/liverpool16.git .
+#    git checkout $BRANCH
+#fi
+
+git clone https://github.com/OCHA-DAP/liverpool16.git .
+git checkout master
+#$BRANCH
 
 chown -R www-data $SRC_DIR
-su - www-data npm install
-su - www-data bower install
-su - www-data grunt default-no-tests
+su - www-data -c "npm install"
+su - www-data -c "bower install"
+su - www-data -c "grunt default-no-tests"
 
-rsync -avh --delete $SRC_DIR/* $DST_DIR/
+rsync -avh --delete $SRC_DIR/bin/* $DST_DIR/
 
 
 
