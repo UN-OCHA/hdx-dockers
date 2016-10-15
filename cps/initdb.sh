e4157c802de28b6ffd75e850b54d5ba4d675ac89
Serban Teodorescu <teodorescu.serban@gmail.com>
2016-10-15 23:32:30 +0300

    cps: hardcode the service name and port for dbcps and email.

--- cps/initdb.sh
+++ cps/initdb.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 
 # create cps schema and a default user if we have an empty database
-if [ "$(psql -h ${HDX_CPSDB_ADDR} -p ${HDX_CPSDB_PORT} -U cps cps -c '\d')" == "No relations found." ]; then
+if [ "$(psql -h dbcps -p 5432 -U cps cps -c '\d')" == "No relations found." ]; then
     echo "Create empty cps schema"
-    pg_restore -vOx -h ${HDX_CPSDB_ADDR} -p ${HDX_CPSDB_PORT} -U ${HDX_CPSDB_USER} -d ${HDX_CPSDB_DB} /srv/cps_schema_0_5_16.plsql
+    pg_restore -vOx -h dbcps -p 5432 -U ${HDX_CPSDB_USER} -d ${HDX_CPSDB_DB} /srv/cps_schema_0_5_16.plsql
     echo "Create example admin user"
-    psql -h ${HDX_CPSDB_ADDR} -p ${HDX_CPSDB_PORT} -U ${HDX_CPSDB_USER} ${HDX_CPSDB_DB} -f /srv/cps-initial-user.sql
+    psql -h dbcps -p 5432 -U ${HDX_CPSDB_USER} ${HDX_CPSDB_DB} -f /srv/cps-initial-user.sql
 fi
